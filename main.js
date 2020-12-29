0df2c0b767d1be8098f6773a172718a4922f1dc7
Marco <marcoferraioli@live.com>
2020-12-29 09:30:46 +0100

    init

---
+++ main.js
@@ -0,0 +1,33 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const config = require('./config.json')
+let mainWindow
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit()
+})
+
+let ppapi_flash_path
+
+if(process.platform  == 'win32'){
+  ppapi_flash_path = path.join(__dirname, 'pepflashplayer.dll')
+} else if (process.platform == 'linux') {
+  ppapi_flash_path = path.join(__dirname, 'libpepflashplayer.so')
+} else if (process.platform == 'darwin') {
+  ppapi_flash_path = path.join(__dirname, 'PepperFlashPlayer.plugin')
+}
+
+app.commandLine.appendSwitch('ignore-certificate-errors', true)
+app.commandLine.appendSwitch('ppapi-flash-path', ppapi_flash_path)
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    'webPreferences': {
+      'plugins': true,
+      'nodeIntegration': true
+    }, 
+  })
+  mainWindow.maximize()
+  mainWindow.loadURL(config.url)
+})
